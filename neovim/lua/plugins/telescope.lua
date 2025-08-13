db880623fe51c338fba133a0f91954c90d56c588
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-13 11:25:17 -0400

    added telescop since todo depended on it

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -1,15 +1,10 @@
 -- -- https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#installation
 -- --
--- return {
--- 	{
--- 		"nvim-telescope/telescope.nvim",
--- 		name = "telescope",
--- 		dependencies = { "nvim-lua/plenary.nvim" },
--- 		opts = {},
--- 	},
--- }
-
-return {}
--- I realized I am not using telescope, removed it from main config
--- But keeping it in my dot files here incase I want to install it again.
--- Snacks.picker already does all the work for me
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		name = "telescope",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+}
