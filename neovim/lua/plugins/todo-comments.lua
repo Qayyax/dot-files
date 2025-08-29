a910ee38fb3b037ed8ac064cbb166fa1045d343c
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-29 10:13:53 -0400

    going to use stow to push changes now

--- neovim/lua/plugins/todo-comments.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {},
-	vim.keymap.set("n", "<leader>tl", ":TodoTelescope<CR>"),
-
-	-- keywords are:
-	-- TODO, HACK, WARN, PERF, NOTE, TEST
-}
--- neovim/lua/plugins/ts-autotags.lua
+++
@@ -1,6 +0,0 @@
--- Docs: https://github.com/windwp/nvim-ts-autotag?tab=readme-ov-file
-return {
-	"windwp/nvim-ts-autotag",
-	event = "InsertEnter",
-	opts = {},
-}
